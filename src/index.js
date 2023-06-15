5e44500fbe8754ef8a06c4659637f34bbd230277
OlehKozubJS <oleg.kozub54@gmail.com>
2023-06-16 00:05:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
